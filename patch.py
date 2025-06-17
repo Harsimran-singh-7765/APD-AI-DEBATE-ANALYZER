72c54f78c4e256f9752f11bacf024908634c6765
Harsimran Singh <singhharry7765@gmail.com>
2025-06-17 17:27:46 +0530

    Update patch.py

--- patch.py
+++ patch.py
@@ -3,8 +3,12 @@ import os
 os.environ["CREWAI_DISABLE_MEMORY"] = "1"
 os.environ["CHROMA_DB_IMPL"] = "duckdb"
 
-# Monkey patch Chroma import if still triggered
 import builtins
-builtins.__dict__["__import__"] = lambda name, *args, **kwargs: (
-    None if name.startswith("chromadb") else __import__(name, *args, **kwargs)
-)
+real_import = builtins.__import__
+
+def safe_import(name, *args, **kwargs):
+    if name.startswith("chromadb"):
+        raise ImportError("ChromaDB disabled")
+    return real_import(name, *args, **kwargs)
+
+builtins.__import__ = safe_import
